3b42ec6a7659c062b56715848c44835452835546
Andrew Drach <andrew.drach@yahoo.com>
2018-06-28 22:06:07 -0500

    refactor the file structure, update the linter configuration, React app styling fixes

---
+++ src-client/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// Define the main app
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit
+      {' '}
+      <code>
+        src/App.jsx
+      </code>
+      {' '}
+      and save to reload.
+    </p>
+  </div>
+);
+
+export default App;
